d0fff9bbcd20cd1e0e3a45da6a4daee54f59bf37
Abdullah <myselfabdullah007@gmail.com>
2024-03-25 02:39:19 +0500

    2nd commit

--- PMD_SE_A_Java/app/src/main/java/com/example/pmd_se_a_java/CommunicationBwFragments/ButtonPressListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.pmd_se_a_java.CommunicationBwFragments;
-
-public interface ButtonPressListener {
-    public interface OnButtonPressListener{
-        public void onButtonPressed(String message);
-    }
-}
